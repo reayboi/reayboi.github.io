b1983339c32b43315c874201b3923fedf402902b
reayboi <brandonreay2000@gmail.com>
2026-04-06 00:46:14 +1000

    potential image loading issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/reayboi.github.io/",
   plugins: [react()],
+   resolve: {
+     alias: {
+        '@assets': path.resolve(__dirname, 'src/assets')
+      }
+    },
 })
