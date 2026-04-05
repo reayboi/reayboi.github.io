8ae8aa1e3b6a7486f0545d31fe9ef717d5125298
reayboi <brandonreay2000@gmail.com>
2026-04-06 01:30:13 +1000

    added styles and new post

--- src/content/data.ts
+++ src/content/data.ts
@@ -2,6 +2,12 @@ import type { PostProps } from "../components/post/post";
 
 export const posts: Array<PostProps> = 
 [
+        {
+    "title": "it's been 84 years",
+    "body": "finally deployed and images loading... that was painful.",
+    "date": new Date("April 6, 2026 01:26:00"),
+    "tags": ["sleepy", "grumpy", "tired"]
+    },
     {
     "title": "One small step",
     "body": "Hello World! This is my first blog entry on Bean's Blog. This might be pretty low effort because I've just spent the last 3 or 4 hours building this website. I'm pretty proud of myself for the work here, digital art was all created using Krita and borrowing Corrie's digital art tablet. I am obssessed with the image I created for the background wallpaper. The colours were inspired by the Spelljammer artwork from the D&D sourcebook lol. Let me know if you can spot the prawn constellation that I made. Anyway, this is the first of many, I hope. Talk soon ;)",
