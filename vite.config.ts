d8d4265d6aed6a36f00b76b418e680cb01921409
reayboi <brandonreay2000@gmail.com>
2026-04-06 00:57:04 +1000

    attemp 100000

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/reayboi.github.io/",
   plugins: [react()],
-   resolve: {
-     alias: {
-        '@assets': path.resolve(__dirname, 'src/assets')
-      }
-    },
 })
